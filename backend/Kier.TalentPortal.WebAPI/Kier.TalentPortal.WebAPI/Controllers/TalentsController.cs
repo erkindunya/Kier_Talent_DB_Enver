21a7c326532e55d62c0a153d66199240718fe853
Mohamed Khalil <mohamed.khalil@outlook.com>
2018-05-16 08:08:42 +0100

    Added the Update Talent method

--- backend/Kier.TalentPortal.WebAPI/Kier.TalentPortal.WebAPI/Controllers/TalentsController.cs
+++ backend/Kier.TalentPortal.WebAPI/Kier.TalentPortal.WebAPI/Controllers/TalentsController.cs
@@ -68,6 +68,42 @@ namespace Kier.TalentPortal.WebAPI.Controllers
             return Ok(talent);
         }
 
+        [HttpPut]
+        public IHttpActionResult UpdateTalentRecord([FromBody]Talent talent)
+        {
+         
+            AuthenticationManager authMgr = new AuthenticationManager();
+            using (ClientContext ctx = authMgr.GetAppOnlyAuthenticatedContext(
+                ConfigurationManager.AppSettings["siteUrl"],
+                ConfigurationManager.AppSettings["clientId"],
+                ConfigurationManager.AppSettings["clientSecret"]))
+            {
+                
+                var list = ctx.Web.Lists.GetByTitle(ConfigurationManager.AppSettings["listName"]);
+                ListItemCreationInformation itemCreateInfo = new ListItemCreationInformation();
+                ListItem newItem = list.AddItem(itemCreateInfo);
+                newItem[Constants.Talent_Record_Division] = talent.Division;
+                newItem[Constants.Talent_Record_Business_Stream] = talent.Stream;
+                newItem[Constants.Talent_Record_Business_Unit] = talent.Unit;
+                newItem[Constants.Talent_Record_Area_Head] = talent.AreaHead;
+                newItem[Constants.Talent_Record_Business_Risk] = talent.BusinessRisk;
+                newItem[Constants.Talent_Record_Development_Notes] = talent.Notes;
+                newItem[Constants.Talent_Record_Employee_Id] = talent.EmployeeId;
+                newItem[Constants.Talent_Record_Flight_Risk] = talent.FlightRisk;
+                newItem[Constants.Talent_Record_Function] = talent.Function;
+                newItem[Constants.Talent_Record_Grade] = talent.Grade;
+                newItem[Constants.Talent_Record_Location] = talent.Location;
+                newItem[Constants.Talent_Record_Movement] = talent.Movement;
+                newItem[Constants.Talent_Record_Performance] = talent.Performance;
+                newItem[Constants.Talent_Record_Potential] = talent.Potential;
+
+                newItem.Update();
+                ctx.ExecuteQuery();
+            }
+
+            return Ok(talent);
+        }
+
 
 
     }
