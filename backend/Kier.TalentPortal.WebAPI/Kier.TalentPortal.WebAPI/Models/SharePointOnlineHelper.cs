2cd2bdb13ab894550c7e3824ec980478cb6cae1e
Mokhalil <MK@itqan.me>
2018-05-18 13:29:18 +0100

    Working version. Still work to do

---
+++ backend/Kier.TalentPortal.WebAPI/Kier.TalentPortal.WebAPI/Models/SharePointOnlineHelper.cs
@@ -0,0 +1,43 @@
+﻿using System.Configuration;
+using System.Linq;
+using System.Web.Http;
+using Kier.TalentPortal.WebAPI.Models;
+using Microsoft.SharePoint.Client;
+using OfficeDevPnP.Core;
+
+namespace Kier.TalentPortal.WebAPI.Models
+{
+    public class SharePointOnlineHelper
+
+    {
+        public static ClientContext GetElevatedContext()
+        {
+            var authMgr = new AuthenticationManager();
+            var ctx = authMgr.GetAppOnlyAuthenticatedContext(
+                ConfigurationManager.AppSettings["siteUrl"],
+                ConfigurationManager.AppSettings["clientId"],
+                ConfigurationManager.AppSettings["clientSecret"]);
+            return ctx;
+        }
+
+        public static int RetrieveUserId(string key)
+        {
+            var ctx = SharePointOnlineHelper.GetElevatedContext();
+            using (ctx)
+            {                
+                var user = ctx.Web.EnsureUser(key);
+                ctx.Load(user);
+                ctx.ExecuteQuery();
+                return user.Id;
+            }
+        }
+
+        public static FieldUserValue ResolveUser(string key)
+        {
+            var fieldValue = new FieldUserValue();
+            var userId = RetrieveUserId(key);
+            fieldValue.LookupId = userId;
+            return fieldValue;
+        }
+    }
+}
